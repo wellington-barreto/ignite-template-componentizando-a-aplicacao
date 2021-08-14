774ca07deae1f5f4efd723e861a7f7346e166e7c
Wellington Barreto <wellingtonbrt@gmail.com>
2021-08-13 21:01:25 -0300

    Desafio finalizado

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,38 @@
-export function Content() {
+import { MovieCard } from '../components/MovieCard';
+
+
+interface ContentProps {
+  selectedGenreTitle: string;
+  movies: MovieProps[];
+}
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+
+export function Content({movies,selectedGenreTitle} : ContentProps ) {
   // Complete aqui
+
+ 
+  return (<div className="container">
+  <header>
+    <span className="category">Categoria:<span> {selectedGenreTitle}</span></span>
+  </header>
+
+  <main>
+    <div className="movies-list">
+      {movies.map(movie => (
+        <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+      ))}
+    </div>
+  </main>
+</div>);
 }
